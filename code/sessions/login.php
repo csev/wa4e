fd03ddcd32443181b6ff5a9b864c1b13a6f95901
Charles Severance <csev@umich.edu>
2015-01-25 21:14:32 -0500

    Tweaks

--- code/sessions/login.php
+++ code/sessions/login.php
@@ -21,8 +21,12 @@
 <h1>Please Log In</h1>
 <?php
     if ( isset($_SESSION["error"]) ) {
-        echo('<p style="color:red">Error:'.$_SESSION["error"]."</p>\n");
+        echo('<p style="color:red">'.$_SESSION["error"]."</p>\n");
         unset($_SESSION["error"]);
+    }
+    if ( isset($_SESSION["success"]) ) {
+        echo('<p style="color:green">'.$_SESSION["success"]."</p>\n");
+        unset($_SESSION["success"]);
     }
 ?>
 <form method="post">
