bb916706d4e25657a17d93d2de48d124f374f553
Charles Severance <csev@umich.edu>
2016-02-04 11:25:50 -0500

    Remove javascript

--- code/rps/bootstrap.php
+++ code/rps/bootstrap.php
@@ -5,9 +5,6 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
 <!-- Custom styles for this template -->
 <link href="starter-template.css" rel="stylesheet">
 
