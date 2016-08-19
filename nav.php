4689f15b24ab5ea30a25e9a3836d4ac477165d5d
Charles Severance <csev@umich.edu>
2016-08-18 21:12:56 -0600

    Add discussion

--- nav.php
+++ nav.php
@@ -6,6 +6,7 @@ $set->setHome($CFG->servicename, $CFG->apphome);
 $set->addLeft('Install', $R.'install.php');
 $set->addLeft('Lessons', $R.'lessons.php');
 if ( isset($_SESSION['id']) ) {
+	$set->addLeft('Discuss', $R.'discuss.php');
 	$set->addLeft('Tools', $R.'lessons.php?anchor=tools');
 }
 
