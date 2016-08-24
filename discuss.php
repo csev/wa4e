e51a3cddc45f6fe236f3e2ad3adcf90bbf3a7795
Charles Severance <csev@umich.edu>
2016-08-24 09:08:30 -0400

    Add the shortname

--- discuss.php
+++ discuss.php
@@ -24,7 +24,7 @@ var disqus_config = function () {
 };
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = '//php-intro.disqus.com/embed.js';
+    s.src = '//<?= $CFG->disqusshortname ?>.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
