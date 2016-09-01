cb6f18fbc724226124e759f58a125d9e4e65f28a
Charles Severance <csev@umich.edu>
2016-09-01 09:10:37 -0400

    Tweak the index.php

--- index.php
+++ index.php
@@ -7,14 +7,62 @@ require_once "nav.php";
 ?>
 <div id="container">
 <h1>Web Applications for Everybody (WA4E)</h1>
+<?php if ( isset($_SESSION['id']) ) { ?>
+<p>
+Welcome to our Massive Open Online Course (MOOC). Now that you have logged in, you have access to 
+course-style features of this web site.
+<ul>
+<li>
+As you go through the <a href="tsugi/lessons.php">Lessons</a> in the course you now will see additional
+links to the autograders in the class.  You can attempt the autograders and get a score.</li>
+<li>
+You can track your progress through the course using the <a href="tsugi/assignments.php">Assignments</a>
+tool and when you complete a group of assignments, you can earn a <a href="tsugi/badges.php">Badge</a>.
+You can download these badges and host them on your web site or refer the badge URLs on this site.</li>
+<li>There is an 
+<a href="https://disqus.com/home/channel/webapplicationsforeverybody/" target="_blank">online disucsson forum</a>
+hosted by Disqus.</li>
+<li>If you want to use these Creative Commons Licensed materials in your own classes you can download
+or link to the artifacts, 
+<a href="tsugi/cc/export.php">export the course material</a> as an 
+<a href="https://www.imsglobal.org/cc/index.html" target="_blank">
+IMS Common Cartridge®</a>, or apply for 
+an <a href="tsugi/admin/key/index.php">IMS LTI key and secret</a> to launch the autograders from your LMS.
+</li>
+</ul>
+<?php } else { ?>
 <p>
 Hello and welcome to my <b>"Web Applications for Everybody"</b> site where you can work through my materials 
 and learn PHP, MySQL, JQuery, and Handlebars.
 </p>
 <p>
-You can submit homework to the autograders for this site if 
-you are logged in.
-I would love to hear from you if you find this material useful.  
+You can use this web site many different ways:
+<ul>
+<li>You browse my videos and course materials under <a href="tsugi/lessons.php">Lessons</a>.  The materials
+I have developed
+for this class are all provided with a Creative Commons license so you can download or link to 
+them to incorporate them into your own teaching if you like.</li>
+<li>This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> framework to embed a learning 
+management system into this site.  So if you <a href="tsugi/login.php">log in</a> to this site 
+it is as if you have joined a free, global
+open and online course.  You have a grade book, autograded assignments, a discussion forum, and can earn
+badges for your efforts.</li>
+<li> This site supports the <a href="https://www.imsglobal.org/cc/index.html" target="_blank">
+IMS Common Cartridge® Specification</a> that allows you to extract all of the material from the course
+and import it into a Learning Management system like 
+<a href="http://www.sakaiproject.org" target="_blank">Sakai</a>, Moodle, Canvas, Blackboaard, BrightSpace, or others.
+You can download a cartridge using the <a href="tsugi/cc/export.php">export feature</a>.  You will also need
+an <a href="tsugi/admin/key/index.php">IMS LTI key and secret</a> to launch the autograders from your LMS.
+</li>
+<li>The code for this site including the autograders, slides, and course content is all available on
+<a href="https://github.com/csev/php-intro" target="_blank">GitHub</a>.  That means you could make your own
+copy of the course site, publish it and remix it any way you like.  Even more exciting, you could translate
+the entire site (course) into your own language and publish it.</li>
+</ul>
+<?php } ?>
+I think that this is an exciting way to provide online materials.  If you are interested in collaborating
+to build these kinds of sites for yourself, please see the 
+<a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
 </p>
 <h3>TextBook</h3>
 <p>
@@ -30,32 +78,12 @@ of the book are free.   Print copies of the book are available on
 <a href="http://www.amazon.com/The-Missing-Link-Introduction-Development/dp/1502447967/"
 target="_blank">Amazon</a>.
 </p>
-<p>
-Video lectures can be found on my
-<a href="https://www.youtube.com/playlist?list=PLlRFEj9H3Oj5F-GFxG-rKzORVAu3jestu" target="_blank">
-PHP/MySql YouTube Channel</a> and
-you can also listen to the audio recordings of the live lectures for 
-<a href="https://archive.org/details/201509UMSI664Podcasts" target="_blank">UMSI 664 Fall 2015</a>
-on campus.
-</p>
 <h3>Copyright</h3>
 <p>
 All this material produced by Charles Severance (including audio and video) 
 is Copyright Creative Commons Attribution 3.0 
 unless otherwise indicated.  
 </p>
-<h3>Technology Notes</h3>
-<p>
-For this course I prepare slides in PowerPoint, record audio using a
-<a href="http://www.sandisk.com/products/sansa-music-and-video-players/sandisk-sansa-clip-zip-mp3-player" target="_blank">Sansa Clip</a>, use iTunes to convert the audio to MP3, use
-Camtasia for my screencasts, 
-and use 
-<a href="http://www.parallels.com/" target="_blank">Parallels</a> to run Windows.
-</p>
-<p>
-This material is always in progress and is revised everytime I teach the course.   
-You can see all of the material and its history on 
-<a href="https://github.com/csev/php-intro" target="_blank">GitHub</a>.
 <p>
 -- Dr. Chuck
 </p>
