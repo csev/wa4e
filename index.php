df5d7b7938767890a75e85f91e8b8c8e5c20da29
Charles Severance <csev@umich.edu>
2016-09-15 21:15:08 -0400

    Tweaks

--- index.php
+++ index.php
@@ -105,4 +105,13 @@ var_dump($USER);
 ?>
 -->
 </div>
-<?php $OUTPUT->footer();
+<?php 
+
+$foot = '
+<p style="font-size: 75%; margin-top: 5em;">
+Copyright Creative Commons Attribution 3.0 - Charles R. Severance
+</p>';
+
+$OUTPUT->setAppFooter($foot);
+
+$OUTPUT->footer();
