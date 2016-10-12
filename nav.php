00a8f849c9f7338af9ec83ef711132f59842f933
Charles Severance <csev@umich.edu>
2016-10-12 16:37:16 -0400

    Add ink to class-central

--- nav.php
+++ nav.php
@@ -29,6 +29,7 @@ if ( isset($_SESSION['id']) ) {
     if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
         $submenu->addLink('Administer', $T . 'admin/');
     }
+    $submenu->addLink('Rate this course', 'https://www.class-central.com/mooc/7362/web-applications-for-everybody');
     $submenu->addLink('Logout', $T.'logout.php');
     if ( isset($_SESSION['avatar']) ) {
         $set->addRight('<img src="'.$_SESSION['avatar'].'" style="height: 2em;"/>', $submenu);
