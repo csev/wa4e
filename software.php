d5b142c7217bddbc6d31bca61e8ddb203ef9bff7
Charles Severance <csev@umich.edu>
2017-01-16 16:35:09 -0500

    Tweak

--- software.php
+++ software.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-     "DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html>
 <head>
 <?php include("header.php"); ?>
 </head>
@@ -17,31 +15,9 @@ We have separate pages for each of the commonly used Operating Systems:
 <li><a href="software-mac.php">Setting up the MAMP PHP/MySql Environment on a Macintosh</a>
 </ul>
 <p>
-You can use any software you like - I suggest XAMPP for Windows and MAMP for Macintosh because it seems as that these
+You can use any software you like - I suggest XAMPP for Windows and MAMP for 
+Macintosh because it seems as that these
 install most reliably and with the least tweaking.
 </p>
-<p>
-You will need 
-<a href="http://www.apple.com/quicktime" target=_new>QuickTime</a> 
-(or iTunes)
-installed on your computer to view any video materials or screencasts.
-You should probably download the high quality copies of these
-files or screencasts to your computer 
-and view/play them 
-locally.  They are rather large files and you will want to move 
-back and forth as well as start and stop the podcasts so you can perform
-the steps as indicated.
-</p>
-</div>
-<!-- Don't use footer because we don't want chat -->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-423997-6");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
 </body>
 </html>
