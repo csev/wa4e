f33f489ea295c5e3c8dec4c7fe7e8d9c87d81fd3
Charles Severance <csev@umich.edu>
2017-02-05 20:37:41 -0500

    Simplify

--- code/sessions/index.php
+++ code/sessions/index.php
@@ -1,14 +1,5 @@
 <?php
     session_start();
-    if ( isset($_POST["street"]) && isset($_POST["city"]) &&
-        isset($_POST["state"]) && isset($_POST["zip"]) ) {
-        $_SESSION['street'] = $_POST['street'];
-        $_SESSION['city'] = $_POST['city'];
-        $_SESSION['state'] = $_POST['state'];
-        $_SESSION['zip'] = $_POST['zip'];
-        header( 'Location: index.php' ) ;
-        return;
-    }      
 ?>
 <html>
 <head>
@@ -21,31 +12,12 @@
         unset($_SESSION["success"]);
     }  
  
-    // Retrieve data form the session for the view
-    $street = isset($_SESSION['street']) ? $_SESSION['street'] : '';
-    $city = isset($_SESSION['city']) ? $_SESSION['city'] : '';
-    $state = isset($_SESSION['state']) ? $_SESSION['state'] : '';   
-    $zip = isset($_SESSION['zip']) ? $_SESSION['zip'] : '';  
-
-    // If we are not logged in, give the user a link to the
-    // login screen.  If we are logged in, put up the 
-    // edit screen.
+    // Check if we are logged in!
     if ( ! isset($_SESSION["account"]) ) { ?>
-       Please <a href="login.php">Log In</a> to start.
+       <p>Please <a href="login.php">Log In</a> to start.</p>
     <?php } else { ?>
-        <p>Please enter your address:
-        <form method="post">
-        <p>Street: <input type="text" name="street" size="50" 
-        value="<?= htmlentities($street) ?>"></p>
-        <p>City: <input type="text" name="city" size="20" 
-        value="<?= htmlentities($city) ?>"></p>
-        <p>State: <input type="text" name="state" size="2" 
-        value="<?= htmlentities($state) ?>">
-        Zip: <input type="text" name="zip" size="5" 
-        value="<?= htmlentities($zip) ?>"></p>
-        <p><input type="submit" value="Update">
-        <input type="button" value="Logout"
-        onclick="location.href='logout.php'; return false"></p>
-        </form>
+       <p>This is where a cool application would be.</p>
+       <p>Please <a href="logout.php">Log Out</a> when you are done.</p>
     <?php } ?>
 </body>
+</html>
