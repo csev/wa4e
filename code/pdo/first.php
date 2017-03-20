d24102035f8e2e3e24ea8a8c282e6e72acabc7db
Charles Severance <csev@umich.edu>
2017-03-20 15:44:01 -0400

    Updates

--- code/pdo/first.php
+++ code/pdo/first.php
@@ -2,9 +2,11 @@
 echo "<pre>\n";
 $pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc',
     'fred', 'zap');
+
 $stmt = $pdo->query("SELECT * FROM users");
+
+// Retrieve a row and check for false in one statement
 while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
     print_r($row);
 }
 echo "</pre>\n";
-?>
