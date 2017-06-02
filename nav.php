87205c40bea40409597fdf1be32c0367eb806676
Charles Severance <csev@umich.edu>
2017-06-02 18:56:31 -0400

    Commit to Koseu URLs

--- nav.php
+++ nav.php
@@ -7,14 +7,13 @@ $set = new \Tsugi\UI\MenuSet();
 $set->setHome($CFG->servicename, $CFG->apphome);
 $set->addLeft('Get Started', $R.'install');
 if ( isset($CFG->lessons) ) {
-    // $set->addLeft('Lessons', $T.'lessons.php');
-    $set->addLeft('Lessons', $T.'lessons');
+    $set->addLeft('Lessons', $R.'lessons');
 }
 $set->addLeft('YouTube', 'https://www.youtube.com/playlist?list=PLlRFEj9H3Oj7FHbnXWviqQt0sKEK_hdKX');
 if ( isset($_SESSION['id']) ) {
 	if ( isset($CFG->disqushost) ) $set->addLeft('Discuss', $T.'discuss.php');
 	else if ( isset($CFG->disquschannel) ) $set->addLeft('Discuss', $CFG->disquschannel);
-	$set->addLeft('Assignments', $T.'assignments.php');
+	$set->addLeft('Assignments', $R.'assignments');
 } else {
     $set->addLeft('Materials', $R.'materials');
 }
@@ -22,23 +21,23 @@ if ( isset($_SESSION['id']) ) {
 
 if ( isset($_SESSION['id']) ) {
     $submenu = new \Tsugi\UI\Menu();
-    $submenu->addLink('Profile', $T.'profile.php');
+    $submenu->addLink('Profile', $R.'profile');
     if ( isset($CFG->google_map_api_key) ) {
-        $submenu->addLink('Map', $T.'map.php');
+        $submenu->addLink('Map', $R.'map');
     }
-    $submenu->addLink('Badges', $T.'badges.php');
+    $submenu->addLink('Badges', $R.'badges');
     $submenu->addLink('Materials', $R.'materials');
     if ( $CFG->DEVELOPER ) {
-        $submenu->addLink('Test LTI Tools', $T . 'dev.php');
+        $submenu->addLink('Test LTI Tools', $T . 'dev');
     }
     if ( $CFG->providekeys ) {
-        $submenu->addLink('LMS Integration', $T . 'admin/key/index.php');
+        $submenu->addLink('LMS Integration', $T . 'admin/key/index');
     }
     if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
         $submenu->addLink('Administer', $T . 'admin/');
     }
     $submenu->addLink('Rate this course', 'https://www.class-central.com/mooc/7362/web-applications-for-everybody');
-    $submenu->addLink('Logout', $T.'logout.php');
+    $submenu->addLink('Logout', $R.'logout');
     if ( isset($_SESSION['avatar']) ) {
         $set->addRight('<img src="'.$_SESSION['avatar'].'" style="height: 2em;"/>', $submenu);
         // htmlentities($_SESSION['displayname']), $submenu);
