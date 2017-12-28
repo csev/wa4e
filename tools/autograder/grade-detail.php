b997f5a8867e2be0a4f588c12862a807f58c2e26
Charles Severance <csev@umich.edu>
2017-12-28 13:13:39 -0500

    Small double check

--- tools/autograder/grade-detail.php
+++ tools/autograder/grade-detail.php
@@ -2,9 +2,14 @@
 require_once "../config.php";
 \Tsugi\Core\LTIX::getConnection();
 
+use \Tsugi\Util\U;
 use \Tsugi\Grades\GradeUtil;
 
 session_start();
+
+if ( ! U::get($_REQUEST, 'user_id') ) {
+    die_with_error_log('user_id not specified');
+}
 
 // Get the user's grade data also checks session
 $row = GradeUtil::gradeLoad($_REQUEST['user_id']);
