e3a8ee3e3c65d3a5eca86cebcfd03c1ed8d0a9d6
Charles Severance <csev@umich.edu>
2021-06-11 16:21:05 -0400

    Updates

--- index.php
+++ index.php
@@ -48,37 +48,33 @@ an IMS Learning Tools Interoperability® (LTI®)
 <p>
 Hello and welcome to my site where you can learn to build database-backed
 web sites using PHP, MySQL, JQuery, and Handlebars. 
-You can use this web site many different ways:
+You can take this course and receive a certificate at:
 <ul>
-<li>
-You browse my videos and course materials under <a href="lessons">Lessons</a>.  The materials
-I have developed
-for this class are all provided with a Creative Commons license so you can download or link to 
-them to incorporate them into your own teaching if you like.</li>
-<li>
-If you <a href="tsugi/login.php">log in</a> to this site 
-it is as if you have joined a free, global
-open and online course.  You have a grade book, autograded assignments, a discussion forum, and can earn
-badges for your efforts.</li>
-<li>
+<li><a href="https://www.coursera.org/specializations/web-applications" target="_blank">Coursera: Web Applications for Everybody Specialization</a> </li>
+</ul>
+If you <a href="tsugi/login.php">log in</a> to this site
+you have joined a free, global
+open and online course.  You have a grade book, autograded assignments, discussion forums, and can earn
+badges for your efforts.</p>
+<p>
 We take your privacy seriously on this site, you can review our
 <a href="privacy">Privacy Policy</a> for more details.
-</li>
-<li>
-If you want to use these Creative Commons Licensed materials 
+</p>
+<p>
+If you want to use these materials
 in your own classes you can download or link to the artifacts on this site,
 <a href="tsugi/cc/">export the course material</a> as an
 IMS Common Cartridge®, or apply for
 an IMS Learning Tools Interoperability® (LTI®)
-<a href="tsugi/admin/key/index.php">key and secret</a> 
+<a href="tsugi/admin/key/index.php">key and secret</a>
  to launch the autograders from your LMS.
-</li>
-<li>
-The <a href="code" target="_blank">sample code</a> for this site including the autograders, slides, and course content is all available on
+</p>
+<p>
+The code for this site including the autograders, slides, and course content is all available on
 <a href="https://github.com/csev/wa4e" target="_blank">GitHub</a>.  That means you could make your own
 copy of the course site, publish it and remix it any way you like.  Even more exciting, you could translate
-the entire site (course) into your own language and publish it.</li>
-</ul>
+the entire site (course) into your own language and publish it.  
+</p>
 <?php } ?>
 This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> 
 framework to embed a learning 
