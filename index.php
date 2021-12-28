e98e274fcb28391010f6a97ec2da53d526538001
Charles Severance <csev@umich.edu>
2021-12-28 08:30:09 -0500

    Aff free code camp link

--- index.php
+++ index.php
@@ -50,6 +50,7 @@ web sites using PHP, MySQL, JQuery, and Handlebars.
 You can take this course and receive a certificate at:
 <ul>
 <li><a href="https://www.coursera.org/specializations/web-applications" target="_blank">Coursera: Web Applications for Everybody Specialization</a> </li>
+<li><a href="https://www.youtube.com/watch?v=xr6uZDRTna0" target="_blank">FreeCodeCamp: Web Applications for Everybody</a>
 <li><a href="https://online.umich.edu/series/web-applications-for-everybody/" target="_blank">Free certificates for University of Michgan students and staff</a></li>
 </ul>
 If you <a href="tsugi/login.php">log in</a> to this site
