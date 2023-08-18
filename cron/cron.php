b86d0a5688a3611c56d5e5da2cb28f8c8ef99104
Charles Severance <csev@umich.edu>
2023-08-17 21:29:27 -0400

    Add simple cron capability

---
+++ cron/cron.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Content-Type: text/plain; charset=utf-8");
+
+$ZIP_TIME = 15;
+
+$dirname = dirname(__DIR__);
+require_once $dirname."/tsugi/config.php";
+
+$marker_file = __DIR__."/cron.txt";
+
+$now = time();
+if ( file_exists($marker_file) ) {
+   $file_time = filemtime($marker_file);
+   $delta = time() - $file_time;
+} else {
+    $delta = 24*60*60;
+}
+
+if ( $delta > $ZIP_TIME ) {
+    echo("Running command\n");
+    echo(system("cd ../code ; bash zips.sh"));
+    echo("\n");
+    touch($marker_file);
+} else {
+    echo("Delta $delta\n");
+}
+
+
