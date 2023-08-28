417cbb832274db71613badc9ce1140ae33ba26b3
Charles Severance <csev@umich.edu>
2023-08-28 10:24:28 -0400

    Clean up some legacy Silex stuff

--- tools/autograder/tsugi.php
+++ tools/autograder/tsugi.php
@@ -2,19 +2,6 @@
 
 require_once('../config.php');
 
-// Make PHP paths pretty .../install => install.php
-$router = new Tsugi\Util\FileRouter();
-$file = $router->fileCheck();
-if ( $file ) {
-    require_once($file);
-    return;
-}
+$tool = new \Tsugi\Core\Tool();
+$tool->run();
 
-// Make a Tsugi Application
-$launch = \Tsugi\Core\LTIX::requireData();
-$app = new \Tsugi\Silex\Application($launch);
-
-// Add some routes
-\Tsugi\Controllers\Analytics::routes($app);
-
-$app->run();
---
+++ tools/cookies/register.php
@@ -0,0 +1,10 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Cookie Autograder",
+"FontAwesome" => "fa-cookie",
+"short_name" => "Cookie Autograder",
+"description" => "This is an autograder for PHP assignments from www.wa4e.com.",
+"messages" => array("launch", "launch_grade"),
+);
+
