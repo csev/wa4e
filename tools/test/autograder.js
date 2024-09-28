af8ed9c298dd1a0d283bf6266e48491b86209c59
Charles Severance <csev@umich.edu>
2024-09-28 15:16:49 +0000

    Progress.

--- tools/test/autograder.js
+++ tools/test/autograder.js
@@ -8,8 +8,39 @@ var findElements = function(tag, text) {
       found.push(elements[i]);
     }
   }
-  
   return found;
+}
+
+var clickRadio = function(label) {
+	var inputs = document.getElementsByTagName('input');
+
+ 	for (let i = 0; i < inputs.length; i++) {
+    	    if (inputs[i].type != 'radio' ) continue;
+	    if ( typeof(inputs[i].id) == 'undefined' ) continue;
+	    var el = document.querySelector('[for="'+inputs[i].id+'"]');
+            console.log('label ',el);
+            console.log('textContent', el.textContent);
+            if ( el.textContent == label ) {
+		console.log('Clicking', el);
+		el.click();
+		return true;
+	     }
+	}
+	return false;
+}
+
+var clickSubmit = function(label) {
+	var inputs = document.getElementsByTagName('input');
+
+ 	for (let i = 0; i < inputs.length; i++) {
+    	    if (inputs[i].type != 'submit' ) continue;
+            if ( inputs[i].value == label ) {
+		console.log('Clicking', inputs[i]);
+		inputs[i].click();
+		return true;
+	     }
+	}
+	return false;
 }
 
 window.addEventListener(
@@ -28,6 +59,37 @@ window.addEventListener(
 	console.log('returning', retval);
 	retval[0].click();
         event.source.postMessage('42', event.origin);
+	return;
+    }
+    if ( event.data.command == 'searchfor' ){
+	const retval = (document.documentElement.textContent || document.documentElement.innerText).indexOf(event.data.text) > -1
+	console.log('returning', retval);
+        event.source.postMessage(retval, event.origin);
+	return;
+    }
+    if ( event.data.command == 'searchforwarn' ){
+	const retval = (document.documentElement.textContent || document.documentElement.innerText).indexOf(event.data.text) > -1
+	console.log('returning', retval);
+        event.source.postMessage(retval, event.origin);
+	return;
+    }
+    if ( event.data.command == 'searchfornot' ){
+	const retval = ! ((document.documentElement.textContent || document.documentElement.innerText).indexOf(event.data.text) > -1);
+	console.log('returning', retval);
+        event.source.postMessage(retval, event.origin);
+	return;
+    }
+    if ( event.data.command == 'clickradio' ){
+	const retval = clickRadio(event.data.text);
+	console.log('returning', retval);
+        event.source.postMessage(retval, event.origin);
+	return;
+    }
+
+    if ( event.data.command == 'clicksubmit' ){
+	const retval = clickSubmit(event.data.text);
+	console.log('returning', retval);
+        event.source.postMessage(retval, event.origin);
 	return;
     }
   },
--- tools/test/test_index.php
+++ tools/test/test_index.php
@@ -1,25 +1,116 @@
 <h1>Test index</h1>
+<!--
 <button onclick="
 document.getElementById('myframe').contentWindow.postMessage(
 	{'command': 'getdomsize'}, 'https://djtutorial.dj4e.com/polls4/');"
 >getdomsize</button>
+
 <button onclick="
 document.getElementById('myframe').contentWindow.postMessage(
 {'command': 'clicklink','tag': 'a', 'text':'Answer to the Ultimate Question'}, 'https://djtutorial.dj4e.com/polls4/');"
 >clicklink</button>
 
+<button onclick="
+document.getElementById('myframe').contentWindow.postMessage(
+{'command': 'searchtext','text': 'Ultimate'}, 'https://djtutorial.dj4e.com/polls4/');"
+>searchtext</button>
+
+<button onclick="
+document.getElementById('myframe').contentWindow.postMessage(
+{'command': 'searchtext','text': 'Waldo'}, 'https://djtutorial.dj4e.com/polls4/');"
+>searchtext 2</button>
+
+<button onclick="
+document.getElementById('myframe').contentWindow.postMessage(
+{'command': 'clickradio','text': '42'}, 'https://djtutorial.dj4e.com/polls4/');"
+>clickradio</button>
+
+<button onclick="
+document.getElementById('myframe').contentWindow.postMessage(
+{'command': 'clicksubmit','text': 'Vote'}, 'https://djtutorial.dj4e.com/polls4/');"
+>clicksubmit</button>
+-->
+
+<button onclick="doNextStep();" id="nextstep">Next Step</button>
+<span id="stepinfo">
+Placeholder
+</span>
+
 <br/>
-<iframe src="https://djtutorial.dj4e.com/polls4/" id="myframe">
+<center>
+<iframe style="width:95%; height=800px;" src="https://djtutorial.dj4e.com/polls4/" id="myframe">
 </iframe>
+</center>
 
 
 <script>
+var baseurl = "https://djtutorial.dj4e.com/polls4/";
+var currenturl = "https://djtutorial.dj4e.com/polls4/";
+
+var currentStep = -1;
+var json = `
+	{
+    "steps" : [
+        {"command": "switchurl", "text": "/owner" },
+        {"command": "searchfor", "text": "Hello"},
+        {"command": "searchforwarn", "text": "A12345"},
+        {"command": "switchurl", "text": "/"},
+        {"command": "searchfor", "text": "Answer to the Ultimate Question"},
+        {"command": "clicklink", "tag": "a", "text":"Answer to the Ultimate Question"},
+        {"command": "searchfor", "text": "Vote"},
+        {"command": "searchfor", "text": "42"},
+        {"command": "clickradio", "text": "42"},
+        {"command": "clicksubmit", "text": "Vote"},
+        {"command": "searchfor", "text": "Vote again?"}
+    ]
+}`;
+var obj = JSON.parse(json);
+var steps = obj.steps;
+
 window.addEventListener(
   "message",
   (event) => {
-    console.log('in parent', event);
+    const step = steps[currentStep];
+    console.log('in parent', event, step);
+    // Processing here...
+    moveToNextStep();
   },
   false,
 );
 
+function moveToNextStep() {
+
+    if ( currentStep >= (steps.length-1) ) {
+        document.getElementById('stepinfo').textContent = 'Test complete';
+        document.getElementById('nextstep').disabled = true;
+        return;
+    }
+
+    currentStep++;
+    document.getElementById('stepinfo').textContent = (currentStep+1)+'/'+steps.length+' '+JSON.stringify(steps[currentStep]);
+
+}
+
+moveToNextStep();
+
+function doNextStep() {
+
+    if ( currentStep >= (steps.length-1) ) {
+        document.getElementById('stepinfo').textContent = 'Test complete';
+        document.getElementById('nextstep').disabled = true;
+        return;
+    }
+        const step = steps[currentStep];
+        console.log(step);
+        if ( step.command == 'switchurl' ) {
+                currenturl = (baseurl + step.text).replace('/\/\//','/');
+                console.log('Switching to', currenturl);
+                document.getElementById('myframe').src = currenturl;
+                moveToNextStep();
+                return;
+        }
+	console.log('Sending...', step, currenturl);
+	document.getElementById('myframe').contentWindow.postMessage(step, currenturl);
+	console.log('sent...');
+}
 </script>
