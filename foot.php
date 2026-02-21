ab14410679f1071c28eace7834607b290e45a088
Charles Severance <csev@umich.edu>
2026-02-21 10:53:50 -0500

    Accessibility

--- foot.php
+++ foot.php
@@ -1,8 +1,8 @@
 <?php
 
 $foot = '
-<p style="font-size: 75%; margin-top: 5em;">
-Copyright Creative Commons Attribution 3.0 - Charles R. Severance
+<p style="font-size: 0.875rem; color: #333; margin-top: 5em;">
+Copyright Creative Commons Attribution 4.0 - Charles R. Severance
 </p>';
 
 $OUTPUT->setAppFooter($foot);
